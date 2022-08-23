cb4ef8777e0110984d4a01385d0bec20d839a706
SiddharthAgrawal92 <47270428+SiddharthAgrawal92@users.noreply.github.com>
2022-08-23 21:58:06 +0530

    Day 1 - Intro to NodeJS & OS

---
+++ Sessions/Day 1 - Intro to NodeJS & OS Concepts/OS Props/server.js
@@ -0,0 +1,12 @@
+
+
+
+const os = require('os');
+
+console.log('Current System Architecture', os.arch());
+
+console.log('Current System Free Memory', os.freemem());
+
+console.log('Current System Total Memory', os.totalmem());
+
+console.log('Current System Platform', os.platform());
